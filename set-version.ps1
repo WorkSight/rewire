04f3bc834e5da09d423137de15b6131cc26c428f
Sandy Place <splace@worksight.net>
2018-04-12 22:12:12 +0100

    set-version.ps1, script to set and tag the package.json version.

---
+++ set-version.ps1
@@ -0,0 +1,40 @@
+param(
+  [Parameter(
+    Position = 0
+  )]
+  [String]
+  # Build server and client, then package it up for deploy
+  $version
+)
+
+function hasChanges() {
+  $ooga = git status --porcelain
+  if ($ooga.length -gt 0) {
+    echo "has changes"
+    return 1
+  }
+  return 0
+}
+
+if(hasChanges) {
+  echo "you must commit all of your changes before setting the version"
+  exit -1
+}
+
+function setVersion($packageFile) {
+  $fileContents = Get-Content -Raw $packageFile
+  $package =  [Newtonsoft.Json.Linq.JObject]::Parse($fileContents)
+  $package.version = $version
+  $package.toString() | Set-Content $packageFile
+}
+
+function updateAllVersions() {
+  dir packages/**/*.json | foreach {
+    setVersion $_
+  }
+  get add -A
+  git tag -a $version -m $version
+}
+
+updateAllVersions
+echo "finished updating package.json projects to $version"
