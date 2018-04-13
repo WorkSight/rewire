685a9a2b109a9b10ed4b8daab5d6695144a54399
Sandy Place <splace@worksight.net>
2018-04-13 14:38:20 -0700

    added project clean script.

---
+++ clean.ps1
@@ -0,0 +1,9 @@
+Remove-Item ./node_modules -r -Force
+Remove-Item ./packages/*/node_modules -r -Force
+Remove-Item ./packages/*/dist -r
+Remove-Item ./.fusebox -r
+Remove-Item ./build -r
+
+dir npm-debug.log -r | foreach {rm $_}
+dir yarn-error.log -r | foreach {rm $_}
+# dir ./packages/dist -r | foreach {rm $_ -r}
