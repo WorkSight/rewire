99beeba530b0628e0d01cb179e48f8d449fc49a7
Ryan Osler <Rosler@Worksight.net>
2019-10-03 12:39:33 -0700

    rewire-grid: - Grid toggleableColumns and toggleableColumnsOptions removed from the grid model, and replaced with an "optionsMenu" property. This expects a function that returns an IGridOptionsMenu object, which includes props for the options menu to be rendered in the top right corner of the grid, and items to render in the menu when it is clicked on. A "createColumnsToggleMenuItems" function has been added to GridTypes, that takes an array of columns and an array of column names, and returns an array of IToggleMenuItem's to be displayed in the optionsMenu. This alone can be supplied to the items property of the IGridOptionsMenu object, to gain the previous functionality of the toggleable columns feature. rewire-ui: - ActionMenu and ToggleMenu items now have a "subheader" prop, which puts a subheader before the actual item. This allows for menus with multiple titles, which can more distinctly separate groups of actions or toggles.   - added missing props to ToggleMenu and ActionMenu.   - IToggleMenuItem's now have an onClick property, allowing items to have their click actions defined on an item by item basis, like the action menu.   - IActionMenuItem onClick property now takes an item as a parameter.   -  Action Menu now has an onItemClick prop, allowing to set a general action that applies to all items, or at least items that don't have their own onClick property.   - New "MenuBase" component that is a generalized version of the ActionMenu and ToggleMenu that handles all of the base functionality. The latter menus now render the MenuBase component with a custom item renderer, greatly reducing code overlap.   - New "MixedMenu" component added, that can take items that are of either IActionMenuItem or IToggleMenuItem. It renders the items using the appropriate renderer of that item type.

--- packages/rewire-ui/src/components/ActionMenu.tsx
+++ packages/rewire-ui/src/components/ActionMenu.tsx
@@ -1,43 +1,35 @@
 import * as React              from 'react';
 import {Observe}               from 'rewire-core';
-import classNames              from 'classnames';
-import Button, {ButtonProps}   from '@material-ui/core/Button';
-import Menu, {MenuProps}       from '@material-ui/core/Menu';
 import MenuItem                from '@material-ui/core/MenuItem';
 import ListItemText            from '@material-ui/core/ListItemText';
 import ListItemIcon            from '@material-ui/core/ListItemIcon';
-import ListSubheader           from '@material-ui/core/ListSubheader';
-import Divider                 from '@material-ui/core/Divider';
-import {SvgIconProps}          from '@material-ui/core/SvgIcon';
 import {Theme}                 from '@material-ui/core/styles';
 import LabelIcon               from '@material-ui/icons/LabelOutlined';
-import {isNullOrUndefined}     from 'rewire-common';
+import MenuBase, {
+  IMenuBaseItem,
+  MenuBaseProps,
+  MenuBaseStylesType,
+  IMenuBaseItemRendererProps,
+}                              from './MenuBase';
 import {WithStyle, withStyles} from './styles';
 
-export interface IActionMenuItem {
-  name: string;
-  title: string;
-  icon?: React.ComponentType<SvgIconProps>;
-  divider?: boolean;
-  closeOnClick?: boolean;
+export interface IActionMenuItem extends IMenuBaseItem {
   isExternalLink?: boolean;
   href?: string;
 
-  disabled?(): boolean;
-  onClick?(): void;
+  onClick?(item: IActionMenuItem): void;
 }
 
-const styles = (theme: Theme) => ({
+interface IActionMenuItemRendererProps extends IMenuBaseItemRendererProps {
+  item: IActionMenuItem;
+}
+
+const actionItemRendererStyles = (theme: Theme) => ({
   menuButton: {
-    minWidth: '0px',
   },
   menu: {
   },
   menuTitleContainer: {
-    paddingTop: '8px',
-    paddingBottom: '8px',
-    lineHeight: '1.75',
-    color: 'inherit',
   },
   listItemText: {
   },
@@ -52,126 +44,51 @@ const styles = (theme: Theme) => ({
   menuItemSelected: {
   },
   menuItemDisabled: {
-    opacity: '0.5',
-    cursor: 'default',
   },
 });
 
-interface IActionMenuProps {
-  tooltip?:      string;
-  title?:        string | JSX.Element | (() => JSX.Element);
-  menuId:        string;
-  buttonContent: JSX.Element;
-  buttonProps:   ButtonProps;
-  items:         IActionMenuItem[];
+export type ActionMenuItemRendererProps = WithStyle<MenuBaseStylesType, IActionMenuItemRendererProps>;
+
+export const ActionItemRenderer = React.memo(withStyles(actionItemRendererStyles, React.forwardRef((props: ActionMenuItemRendererProps, ref: any): JSX.Element => {
+  return <Observe render={() => {
+    const item         = props.item;
+    const disabled     = props.disabled;
+    const classes      = props.classes;
+    const rootClasses  = props.rootClasses;
+    const clickHandler = props.clickHandler;
+    let externalLinkProps: any = {};
+    if (item.isExternalLink) {
+        externalLinkProps.component = 'a';
+        externalLinkProps.target    = '_blank';
+        externalLinkProps.href      = item.href || '';
+    }
+    return (
+        <MenuItem key={item.name} {...externalLinkProps} divider={item.divider} disableRipple={disabled} classes={{root: rootClasses, selected: classes.menuItemSelected}} onClick={clickHandler}>
+          <ListItemIcon className={classes.listItemIcon}>
+            {item.icon ? <item.icon /> : <LabelIcon />}
+          </ListItemIcon>
+          <ListItemText className={classes.listItemText} primary={item.title} primaryTypographyProps={{classes: {root: classes.listItemTypography}}} />
+        </MenuItem>
+    );
+  }} />;
+})));
+
+export interface IActionMenuProps extends MenuBaseProps {
+  items: IActionMenuItem[];
+
+  onItemClick?(item: IActionMenuItem): void;
 }
 
-interface IActionMenuState {
-  anchorEl?: HTMLElement;
-}
-
-export type ActionMenuProps = WithStyle<ReturnType<typeof styles>, MenuProps & IActionMenuProps>;
-
-class ActionMenu extends React.Component<ActionMenuProps, IActionMenuState> {
-  state: IActionMenuState = {
-    anchorEl: undefined,
-  };
-
-  constructor(props: ActionMenuProps) {
+class ActionMenu extends React.Component<IActionMenuProps> {
+  constructor(props: IActionMenuProps) {
     super(props);
   }
 
-  handleItemClick = (item: IActionMenuItem) => () => {
-    item.onClick && item.onClick();
-    if (item.closeOnClick) {
-      this.handleMenuClose();
-    }
-  }
-
-  handleMenuClick = (evt: React.MouseEvent<HTMLElement>) => {
-    this.setState({anchorEl: evt.currentTarget});
-  }
-
-  handleMenuClose = () => {
-    this.setState({anchorEl: undefined});
-  }
-
-  renderTitle = React.memo((): JSX.Element | null => {
-    return <Observe render={() => {
-      const {title, classes} = this.props;
-      const titleDisplay = typeof title === 'function' ? title() : title;
-      return (
-        <>
-        <ListSubheader component='div' className={classes.menuTitleContainer}>
-          {titleDisplay}
-        </ListSubheader>
-        </>
-      );
-    }} />;
-  });
-
-  renderMenuContent = React.memo(React.forwardRef((): JSX.Element => {
-    return <Observe render={() => {
-      const {classes, items} = this.props;
-      return (
-        < >
-        {items.map((item: IActionMenuItem) => {
-          let externalLinkProps: any = {};
-          if (item.isExternalLink) {
-              externalLinkProps.component = 'a';
-              externalLinkProps.target    = '_blank';
-              externalLinkProps.href      = item.href || '';
-          }
-          let disabled     = !!(item.disabled && item.disabled());
-          let rootClasses  = classNames(classes.menuItem, disabled ? classes.menuItemDisabled : undefined);
-          let clickHandler = !disabled ? this.handleItemClick(item) : undefined;
-
-          return (
-            <MenuItem key={item.name} {...externalLinkProps} divider={item.divider} disableRipple={disabled} classes={{root: rootClasses, selected: classes.menuItemSelected}} onClick={clickHandler}>
-              <ListItemIcon className={classes.listItemIcon}>
-                {item.icon ? <item.icon /> : <LabelIcon />}
-              </ListItemIcon>
-              <ListItemText className={classes.listItemText} primary={item.title} primaryTypographyProps={{classes: {root: classes.listItemTypography}}} />
-            </MenuItem>
-          );
-        })}
-        </>
-      );
-    }} />;
-  }));
-
   render() {
-    const {classes, menuId, buttonContent, buttonProps, items, marginThreshold, MenuListProps, title, tooltip, ...restProps} = this.props;
-    const {anchorEl} = this.state;
-
     return <Observe render={() => (
-      < >
-        <Button
-          title={tooltip}
-          className={classes.menuButton}
-          aria-owns={anchorEl ? menuId : undefined}
-          aria-haspopup='true'
-          onClick={this.handleMenuClick}
-          {...buttonProps}
-        >
-          {buttonContent}
-        </Button>
-        <Menu
-          id={menuId}
-          className={classes.menu}
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleMenuClose}
-          marginThreshold={!isNullOrUndefined(marginThreshold) ? marginThreshold : 5}
-          disableEnforceFocus={true}
-          MenuListProps={{dense: true, disablePadding: true, subheader: title ? <this.renderTitle /> : undefined, ...MenuListProps}}
-          {...restProps}
-        >
-          <this.renderMenuContent />
-        </Menu>
-      </>
+      <MenuBase itemRenderer={ActionItemRenderer} {...this.props} />
     )} />;
   }
 }
 
-export default withStyles(styles, ActionMenu);
+export default ActionMenu;
