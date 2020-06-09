41926a469a29aaa05a7aa0331c593e0e82888dc8
Sandy Place <splace@worksight.net>
2020-06-09 10:53:07 -0700

    isNullOrUndefined fix to allow better intellisense

--- packages/rewire-common/src/isNullOrUndefined.ts
+++ packages/rewire-common/src/isNullOrUndefined.ts
@@ -1,3 +1,3 @@
-export default function isNullOrUndefined(value: any) {
+export default function isNullOrUndefined(value: any): value is null | undefined {
   return value === undefined || value === null;
 }
