54067f4f69ccd5a053eb61e6d32a514b431167bb
Sandy Place <splace@worksight.net>
2022-06-27 12:10:30 -0700

    optimizations

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+function manualChunks(id) {
+  if (id.includes('@material-ui')) {
+    return 'materialUI'
+  }
+  if (id.includes('react')) {
+    return 'react'
+  }
+  if (id.includes('node_modules')) {
+    return 'vendor';
+  }
+  return 'app';
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()]
 })
